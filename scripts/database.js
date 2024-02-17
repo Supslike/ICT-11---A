1e8ac8937ef6697e8f3370efe94fdaa12a38eea5
Supslike <80964103+Supslike@users.noreply.github.com>
2024-02-18 04:20:06 +0800

    uploaded files

---
+++ scripts/database.js
@@ -0,0 +1,72 @@
+async function DownloadImage(url) {
+    await fetch(url)
+        .then(response => response.blob())
+        .then(blob => {
+            const reader = new FileReader();
+            reader.onload = function() {
+                const imageDataUrl = reader.result;
+                localStorage.setItem("ICT_PFP", imageDataUrl);
+                localStorage.setItem("ICT_INPUTS", [])
+            };
+            reader.readAsDataURL(blob);
+        })
+        .catch(error => console.error('Error downloading image:', error));
+}
+
+function AddName(name) {
+    localStorage.setItem("ICT_NAME", name)
+}
+
+function DefaultPFP() {
+    localStorage.setItem("ICT_PFP", "https://www.pythonanywhere.com/user/Supslikes/files/home/Supslikes/default_pfp.jpg")
+}
+
+function GetImage() {
+    return localStorage.getItem("ICT_PFP");
+}
+
+function hasChanged(oldObj, newObj) {
+    var oldStr = JSON.stringify(oldObj).trim(); // Remove leading and trailing whitespace
+    var newStr = JSON.stringify(newObj).trim(); // Remove leading and trailing whitespace
+
+    if (oldStr !== newStr) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+async function LoadAssignments() {
+    try {
+        const response = await fetch("https://supslikes.pythonanywhere.com/assignment", { 
+            method: 'GET', 
+            headers: { 'Content-Type': 'application/json'} 
+        });
+        if (!response.ok) {
+            throw new Error('Network response FAILED');
+        }
+        const data = await response.text();
+        console.log('Response from server:', data);
+        localStorage.setItem("ICT_TEMP", JSON.stringify(data));
+        if (hasChanged(JSON.parse(localStorage.getItem("ICT_TEMP")), JSON.parse(localStorage.getItem("ICT_DATA")))) {
+            localStorage.setItem("ICT_DATA", JSON.stringify(data))
+            localStorage.setItem("ICT_INPUTS", JSON.stringify([]))
+            var time_check = new Date()
+            var hours = time_check.getHours();
+            var minutes = time_check.getMinutes();
+            var amOrPm = hours >= 12 ? 'PM' : 'AM';
+            hours = hours % 12;
+            hours = hours ? hours : 12;
+            minutes = minutes < 10 ? '0' + minutes : minutes;
+            var timeString = hours + ':' + minutes + ' ' + amOrPm;
+            var cur_date = `${monthNames[time_check.getMonth()]} ${time_check.getDate()}, ${timeString}`
+            localStorage.setItem("ICT_UPDATE", JSON.stringify(cur_date))
+            document.getElementById("last_update").textContent = cur_date;
+        }
+        localStorage.removeItem("ICT_TEMP");
+        return data; 
+    } catch (error) {
+        console.error('There was a problem with the request:', error);
+        throw error;
+    }
+}
---
+++ scripts/homepage.js
@@ -0,0 +1,265 @@
+const PFP = GetImage()
+if (PFP === null) {
+    window.location.href = "index.html";
+}
+document.getElementById("website-ico").href = PFP
+
+const list_container = document.getElementById("todo-list")
+const categories = document.querySelectorAll(".sub-holder")
+
+let TEMP_WORKLOAD = 0;
+let TEMP_DUE = 0;
+let TEMP_DONE = 0;
+let TEMP_TOM = 0
+
+let ASSIGNMENT;
+let ID_ASS;
+
+let WORKLOAD = 0;
+let DUE = 0;
+let DONE = 0;
+let TOM = 0;
+
+let MAX_WORKLOAD = 0;
+let MAX_DUE = 0;
+let MAX_DONE = 0;
+let MAX_TOM = 0;
+
+var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+document.getElementById("profile").src = PFP;
+document.getElementById("username").innerHTML = `Hello, <a style="opacity: 0.7; font-size: 10px; margin-left: 40px;" id="last_update"></a> <h2>${localStorage.getItem("ICT_NAME")}!</h2>`;
+document.getElementById("last_update").textContent = `Last Updated: ${JSON.parse(localStorage.getItem("ICT_UPDATE"))}`;
+document.getElementById("description").innerHTML = `<span class="material-symbols-outlined" style="color: red;">description</span><b>${DUE}</b> works due today, <b>${TOM}</b> due tomorrow! <br> <br><span class="material-symbols-outlined" style="color: orange;">Newspaper</span><b>${WORKLOAD}</b> assigned task! <br> <br> <span class="material-symbols-outlined" style="color: green;">check</span><b>${DONE}</b> finished tasks!`;
+document.getElementById("o").addEventListener("click", function() {
+    localStorage.removeItem("ICT_PFP");
+    localStorage.removeItem("ICT_NAME");
+    window.location.href = "index.html"
+})
+
+function UpdateStats() {
+            TEMP_DONE = 0
+            TEMP_DUE = 0
+            TEMP_WORKLOAD = 0
+            TEMP_TOM = 0
+            let checked_data = JSON.parse(localStorage.getItem("ICT_INPUTS"))
+            for (let i = 0; i < checked_data.length; i++) {
+                TEMP_DONE += 1
+                if (checked_data[i].includes("due")) {
+                    TEMP_DUE += 1
+                }
+
+                else if (checked_data[i].includes("tom")) {
+                    TEMP_TOM += 1
+                }
+                TEMP_WORKLOAD += 1
+            }
+            DONE = MAX_DONE + TEMP_DONE;
+            WORKLOAD = MAX_WORKLOAD - TEMP_WORKLOAD
+            DUE = MAX_DUE - TEMP_DUE
+            TOM = MAX_TOM - TEMP_TOM
+            document.getElementById("description").innerHTML = `<span class="material-symbols-outlined" style="color: red;">description</span>You have <b>${DUE}</b> works due today! <br> <br><span class="material-symbols-outlined" style="color: orange;">Newspaper</span><b>${WORKLOAD}</b> assigned task! <br> <br> <span class="material-symbols-outlined" style="color: green;">check</span><b>${DONE}</b> finished tasks!`;
+
+            const inputs = document.querySelectorAll("input");
+            const labels = document.querySelectorAll("label");
+            for (let i = 0; i < inputs.length; i++) {
+                inputs[i].addEventListener("change", function(index) {
+                    return function() {
+                        if (inputs[index].checked) {
+                            labels[i].className = "checked"
+                            DONE += 1;
+                            WORKLOAD -= 1;
+                            if (inputs[index].id.includes("due")) {
+                                DUE -= 1
+                            }
+                            if (inputs[index].id.includes("tom")) {
+                                TOM -= 1;
+                            }
+                            let checked_inputs = JSON.parse(localStorage.getItem("ICT_INPUTS"))
+                            checked_inputs.push(inputs[i].id);
+                            localStorage.setItem("ICT_INPUTS", JSON.stringify(checked_inputs))
+                        }
+                        else {
+                            labels[i].className = "";
+                            DONE -= 1;
+                            WORKLOAD += 1;
+                            if (inputs[index].id.includes("due")) {
+                                DUE += 1;
+                            }
+                            if (inputs[index].id.includes("tom")) {
+                                TOM += 1;
+                            }
+                            let checked_inputs = JSON.parse(localStorage.getItem("ICT_INPUTS"))
+                            let indexToRemove = checked_inputs.indexOf(inputs[i].id);
+                            checked_inputs.splice(indexToRemove, 1);
+                            localStorage.setItem("ICT_INPUTS", JSON.stringify(checked_inputs))
+                        }
+                        document.getElementById("description").innerHTML = `<span class="material-symbols-outlined" style="color: red;">description</span><b>${DUE}</b> works due today, <b>${TOM}</b> due tomorrow! <br> <br><span class="material-symbols-outlined" style="color: orange;">Newspaper</span><b>${WORKLOAD}</b> assigned task! <br> <br> <span class="material-symbols-outlined" style="color: green;">check</span><b>${DONE}</b> finished tasks!`;
+                    };
+                }(i));
+            }
+        }
+
+function sortDictionaryByNumber(dict) {
+    let keys = Object.keys(dict);
+    keys.sort((a, b) => {
+        let aValue = dict[a];
+        let bValue = dict[b];
+        if (aValue === -1) return -1;
+        if (bValue === -1) return 1;
+        if (aValue >= 0 && bValue >= 0) return aValue - bValue;
+        if (aValue < 0 && bValue < 0) return bValue - aValue;
+        if (aValue >= 0 && bValue < 0) return -1;
+        if (aValue < 0 && bValue >= 0) return 1;
+    });
+    return keys;
+}
+
+function timeRemainingUntil(targetDate) {
+    // Get the current date and time
+    var now = new Date();
+    var difference = targetDate.getTime() - now.getTime();
+    var daysRemaining = Math.floor(difference / (1000 * 60 * 60 * 24));
+    var hoursRemaining = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutesRemaining = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var secondsRemaining = Math.floor((difference % (1000 * 60)) / 1000);
+    return {
+        days: daysRemaining,
+        hours: hoursRemaining,
+        minutes: minutesRemaining,
+        seconds: secondsRemaining,
+        month: monthNames[targetDate.getMonth()],
+        day: targetDate.getDate()
+    };
+}
+
+function GenerateAssignments(data, subject) {
+    if (data.length !== 0) {
+        const SUBJECT = subject;
+        let final = `
+                    <div class="sub">
+                        <h2>{subject}</h2>
+                        <hr>
+                        <div class="task-container">
+        `
+        let sorting = {};
+        for (let i = 0; i < data.length; i++) {
+            const DUE_DATE = timeRemainingUntil(new Date(data[i]["due"]));
+            sorting[i] = Number(DUE_DATE.days)
+        }
+        var idx = 0
+        for (let i = 0; i < Object.keys(ASSIGNMENT).length; i++) {
+            if (subject !== Object.keys(ASSIGNMENT)[i]) {
+                idx += 1
+            }
+
+            else {
+                break
+            }
+        }
+        const ORDER = sortDictionaryByNumber(sorting) 
+        for (let i = 0; i < data.length; i++) {
+            TEMP_WORKLOAD += 1
+            const DESCRIPTION = data[ORDER[i]]["description"];
+            const DUE_DATE = timeRemainingUntil(new Date(data[ORDER[i]]["due"]));
+            let check_default = ""
+            if (JSON.parse(localStorage.getItem("ICT_INPUTS")).includes(String(idx))) {
+                check_default = "checked"
+            }
+            let REMAINING = "";
+            let STATUS = "";
+            let DETAILS = `<input type="checkbox" name="task" ${check_default} id="${idx}">
+            <label for="task" class="${check_default}">{description}</label>
+            <p id="due-date"><span class="material-symbols-outlined" style="color: orange; transform: translateY(6.5px);">calendar_today</span>{deadline}</p>`
+            if (DUE_DATE.days > 1) {
+                REMAINING = `in ${DUE_DATE.days} days`;
+                STATUS = `${DUE_DATE.month} ${DUE_DATE.day},`
+            }
+
+            else if (DUE_DATE.days == 0) {
+                REMAINING = `${DUE_DATE.hours} hours left (TOMORROW)`;
+                STATUS = `${DUE_DATE.month} ${DUE_DATE.day},`;
+                TEMP_TOM += 1
+                if (JSON.parse(localStorage.getItem("ICT_INPUTS")).includes(idx + " tom")) {
+                    check_default = "checked"
+                }
+                DETAILS = `<input type="checkbox" name="task" ${check_default} id="${idx} tom">
+                <label for="task" class="${check_default}">{description}</label>
+                <p id="due-date"><span class="material-symbols-outlined" style="color: orange; transform: translateY(6.5px);">calendar_today</span>{deadline}</p>`
+                }
+
+            else if (DUE_DATE.days == -1) {
+                REMAINING = `(TODAY)`;
+                STATUS = `${DUE_DATE.month} ${DUE_DATE.day},`
+                TEMP_DUE += 1
+                if (JSON.parse(localStorage.getItem("ICT_INPUTS")).includes(idx + " due")) {
+                    check_default = "checked"
+                }
+                DETAILS = `<input type="checkbox" name="task" ${check_default} id="${idx} due">
+                <label for="task" class="${check_default}">{description}</label>
+                <p id="due-date"><span class="material-symbols-outlined" style="color: orange; transform: translateY(6.5px);">calendar_today</span>{deadline}</p>`
+            }
+
+            else {
+                REMAINING = `(PASSED)`;
+                STATUS = `${DUE_DATE.month} ${DUE_DATE.day},`
+            }
+
+            final += `
+
+            <div class="task">
+                {details}
+            </div>
+            <hr>
+            `.replace("{details}", DETAILS).replace("{description}", DESCRIPTION).replace("{deadline}", `${STATUS} ${REMAINING}`);
+            idx += 1
+        }
+        final += "</div> </div>";
+        return final.replace("{subject}", SUBJECT);
+        }
+    else {
+        return ""
+    }
+    }
+
+for (let i = 0; i < categories.length; i++) {
+    categories[i].addEventListener("click", function() {
+        if (categories[i].id.toUpperCase() === "ALL") {
+            let final = ""
+            for (let keys in ASSIGNMENT) {
+                final += GenerateAssignments(ASSIGNMENT[keys], keys)
+            }
+            list_container.innerHTML = final;
+        }
+
+        else {
+            list_container.innerHTML = GenerateAssignments(ASSIGNMENT[categories[i].id.toUpperCase()], categories[i].id.toUpperCase()) 
+        }
+        UpdateStats()
+    })
+}
+
+document.addEventListener("DOMContentLoaded", async function() {
+    const ASSIGNMENTS = JSON.parse(await LoadAssignments());
+    ASSIGNMENT = ASSIGNMENTS
+    list_container.innerHTML = ""
+    let final = ""
+    for (let keys in ASSIGNMENTS) {
+        final += GenerateAssignments(ASSIGNMENTS[keys], keys)
+    }
+    list_container.innerHTML = final;
+    WORKLOAD = TEMP_WORKLOAD;
+    DUE = TEMP_DUE;
+    DONE = TEMP_DONE;
+    TOM = TEMP_TOM
+    MAX_TOM = TOM
+    MAX_DONE = DONE;
+    MAX_WORKLOAD = WORKLOAD;
+    MAX_DUE = DUE
+    UpdateStats()
+})
